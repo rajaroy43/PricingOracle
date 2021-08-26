365a72d453532e48b25cfe140d3a0b201847edb0
mylons <mrlyons@gmail.com>
2021-08-25 20:30:30 -0700

    i think this is closer to what you want

--- docker/reward_coordinator/dist/index.js
+++ docker/reward_coordinator/dist/index.js
@@ -1,11 +1,18 @@
 "use strict";
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var http_1 = __importDefault(require("http"));
+var client_1 = require("@apollo/client");
+var cross_fetch_1 = __importDefault(require("cross-fetch"));
 var app = express_1.default();
+var SCHEMA_QUERY = client_1.gql(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n{\n  __schema {\n    queryType{\n      fields {\n        name\n        description\n      }\n    }\n  }\n}\n"], ["\n{\n  __schema {\n    queryType{\n      fields {\n        name\n        description\n      }\n    }\n  }\n}\n"])));
 app.get('/', (function (req, res) {
     http_1.default.get('http://reward_calculator:9001/ping-me', function (resp) {
         var data = '';
@@ -33,4 +40,17 @@ new request object
 request.post("http://python:8001/ping-me")
 
  */
+app.get('/graphql', (function (req, res) {
+    var client = new client_1.ApolloClient({
+        link: new client_1.HttpLink({ uri: 'http://localhost:8000/subgraphs/name/scaffold-eth/your-contract/graphql', fetch: cross_fetch_1.default })
+        //uri: 'http://localhost:8000/subgraphs/name/scaffold-eth/your-contract/graphql',
+        ,
+        //uri: 'http://localhost:8000/subgraphs/name/scaffold-eth/your-contract/graphql',
+        cache: new client_1.InMemoryCache()
+    });
+    return client.query({
+        query: SCHEMA_QUERY
+    });
+}));
 app.listen(port, function () { return console.log("App listening on PORT " + port); });
+var templateObject_1;
--- docker/reward_coordinator/index.ts
+++ docker/reward_coordinator/index.ts
@@ -1,6 +1,21 @@
 import express from "express"
 import http from "http"
+import { gql, ApolloClient, InMemoryCache, HttpLink  } from '@apollo/client'
+import fetch from 'cross-fetch'
+
 const app = express()
+
+const SCHEMA_QUERY = gql`query {
+  __schema {
+    queryType {
+      fields {
+        name
+        description
+      }
+    }
+  }
+}
+`
 
 app.get('/', ((req, res) => {
     http.get('http://reward_calculator:9001/ping-me', (resp) => {
@@ -21,7 +36,7 @@ app.get('/', ((req, res) => {
     });
 
     res.send("Hello from the node server");
-}))
+}));
 
 const port = 9000;
 
@@ -36,5 +51,39 @@ new request object
 request.post("http://python:8001/ping-me")
 
  */
+app.get('/graphql', ((req, res) => {
+    http.get('http://docker_graph-node_1:8000/subgraphs/name/scaffold-eth/your-contract/graphql?query=query%20%7B%0A%20%20__schema%20%7B%0A%20%20%20%20queryType%7B%0A%20%20%20%20%20%20fields%20%7B%0A%20%20%20%20%20%20%20%20name%0A%20%20%20%20%20%20%20%20description%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D', (resp) => {
+        let data = '';
+
+        // A chunk of data has been received.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            console.log(data);
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
+    // i hate typescript and can't figure out the below to get it to work. says there's an error in the JSON at position 0
+    /*
+    const client = new ApolloClient({
+        link: new HttpLink({ uri: 'http://docker_graph-node_1:8000/subgraphs/name/scaffold-eth/your-contract/graphql', fetch }),
+        //uri: 'http://localhost:8000/subgraphs/name/scaffold-eth/your-contract/graphql',
+        cache: new InMemoryCache()
+    });
+    return client.query({
+        query: SCHEMA_QUERY
+    })
+    .then(result => {
+        console.log(result);
+        return result
+    });
+     */
+}));
+
 
 app.listen(port, () => console.log(`App listening on PORT ${port}`));
