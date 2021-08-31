b34a323c69b718c98cd17eef0c3da69496a691af
dtolsen <dtolsen@pixelranch.io>
2021-08-31 12:04:33 -0500

    Styling button and box elements

--- packages/react-app/src/components/atoms/inputs/buttons/Button.js
+++ packages/react-app/src/components/atoms/inputs/buttons/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 
 const _Button = ({onClick = () => {}, label, type = 'button', disabled = false, variant = 'contained',  style = {}}) =>
-  <Button variant={variant} onClick={onClick} color="primary" type={type} disabled={disabled} style={style}>
+  <Button variant={variant} onClick={onClick} color="primary" type={type} disabled={disabled} style={style} disableElevation>
     {label}
   </Button>
 
