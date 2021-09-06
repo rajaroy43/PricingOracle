fa2ed0db527c036f8a190feb36a075b2f3e24f49
rajaroy43 <rajaroy674@gmail.com>
2021-09-06 21:30:07 +0530

    mock questionsGroup

--- packages/hardhat/scripts/deploy.js
+++ packages/hardhat/scripts/deploy.js
@@ -4,7 +4,7 @@ const chalk = require("chalk");
 const { config, ethers, tenderly, run, network } = require("hardhat");
 const { utils } = require("ethers");
 const R = require("ramda");
-
+const mockQuestionData = require("./utils/mockQuestionData");
 const main = async () => {
   const accounts = await ethers.getSigners();
   account0 = accounts[0];
@@ -29,7 +29,6 @@ const main = async () => {
   const transferBalance = ethers.utils.parseUnits("1000.0", 18);
   if (network.name === "localhost") {
     const approveAmount = ethers.utils.parseUnits("10000000000000.0", 18);
-    //Create QuestionsGroup
     await lithToken.approve(lithiumPricing.address, approveAmount);
     await lithToken
       .connect(account1)
@@ -39,61 +38,13 @@ const main = async () => {
       .approve(lithiumPricing.address, approveAmount);
     await lithToken.transfer(account1.address, transferBalance);
     await lithToken.transfer(account2.address, transferBalance);
+    //Creating mock QuestionsGroup
     await createQuestiongroup(lithiumPricing);
     console.log(chalk.magenta("QuestionGroups created ", "\n"));
   }
 
   async function createQuestiongroup(lithiumPricing) {
-    const block = await ethers.provider.getBlock();
-    const pricingTime = block.timestamp + 700000000000;
-    const endTime = block.timestamp + 500000000000;
-    const description = `What is the price of an MRF share `;
-    const bounty = ethers.utils.parseUnits("80.0", 18);
-    const answerSet = [0, 50];
-    const categoryId = 0;
-
-    const pricingTime1 = block.timestamp + 700000000000;
-    const endTime1 = block.timestamp + 500000000000;
-    const description1 = `What is the price of an TATA NANO share  `;
-    const bounty1 = ethers.utils.parseUnits("120.0", 18);
-    const answerSet1 = [0, 1320];
-    const categoryId1 = 0;
-
-    const pricingTime2 = block.timestamp + 800000000000;
-    const endTime2 = block.timestamp + 200000000000;
-    const description2 = `What is the price of an Lithium Token share  `;
-    const bounty2 = ethers.utils.parseUnits("125.0", 18);
-    const answerSet2 = [0, 9870];
-    const categoryId2 = 0;
-
-    const pricingTime3 = block.timestamp + 1200000000000;
-    const endTime3 = block.timestamp + 300000000000;
-    const description3 = `What is the price of an TESLA-PRE-IPO  share  `;
-    const bounty3 = ethers.utils.parseUnits("183.0", 18);
-    const answerSet3 = [0, 9870];
-    const categoryId3 = 0;
-
-    const questiontype = 0;
-    const args1 = [
-      [categoryId, categoryId1, categoryId2, categoryId3],
-      [bounty, bounty1, bounty2, bounty3],
-      [pricingTime, pricingTime1, pricingTime2, pricingTime3],
-      [endTime, endTime1, endTime2, endTime3],
-      [questiontype, questiontype, questiontype, questiontype],
-      [description, description1, description2, description3],
-      [answerSet, answerSet1, answerSet2, answerSet3],
-    ];
-
-    const args2 = [
-      [categoryId, categoryId1, categoryId2, categoryId3],
-      [bounty, bounty1, bounty2, bounty3],
-      [pricingTime, pricingTime1, pricingTime2, pricingTime3],
-      [endTime, endTime1, endTime2, endTime3],
-      [questiontype, questiontype, questiontype, questiontype],
-      [description, description1, description2, description3],
-      [answerSet, answerSet1, answerSet2, answerSet3],
-    ];
-    const args = [args1, args2];
+    const args = await mockQuestionData();
     console.log("\n\n 📡 Creating question groups \n");
     for (var i = 0; i < args.length; i++) {
       await lithiumPricing.createQuestionGroup(...args[i]);
