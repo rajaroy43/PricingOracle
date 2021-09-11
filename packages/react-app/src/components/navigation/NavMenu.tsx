5c43d3f7635ddd0d2a61a63abfbb5ff92ee750bf
dtolsen <dtolsen@pixelranch.io>
2021-09-11 11:58:22 -0500

    Adding mobile responsive navigation

--- packages/react-app/src/components/navigation/NavMenu.tsx
+++ packages/react-app/src/components/navigation/NavMenu.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import { GetNavItemParams, NavMenuParams, NavItemParams, GetIsActive } from '../../types/navigation'
+import { GetNavItemParams, NavMenuParams, NavItemParams, PageParams } from '../../types/navigation'
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '../atoms/Box'
 import NavMenuItem from './NavMenuItem'
+import { SwipeableDrawer, useMediaQuery, useTheme } from '@material-ui/core';
+import NavMenuItemMobile from './NavMenuItemMobile';
 
-const  prepareNavItem = (
-  menuProps: NavMenuParams, itemProps: GetNavItemParams
-): NavItemParams => {
+
+const useStyles = makeStyles(theme => ({
+  navMenus: {
+    margin: 0,
+    padding: '16px 0 0 0'
+  },
+  navMenuMobile: {
+    backgroundColor: '#222222',
+    padding: '16px'
+  },
+  spacer: {
+    marginTop: '64px'
+  }
+})); 
+
+const  prepareNavItem = (menuProps: PageParams, itemProps: GetNavItemParams): NavItemParams => {
   return {
     id: itemProps.id,
     icon: itemProps.icon,
@@ -15,26 +30,46 @@ const  prepareNavItem = (
     shouldRender: itemProps.getShouldRender(menuProps),
     isActive: itemProps.getIsActive(menuProps, itemProps),
   }
-}
+} 
 
-const useStyles = makeStyles(theme => ({
-  navMenus: {
-    margin: 0,
-    padding: '16px 0 0 0'
-  },
-}));  
-
-const NavMenu = (props: NavMenuParams) => {  
+const NavMenu = (props: NavMenuParams) => {
   const classes = useStyles();
+  const theme = useTheme();
+  const iOS = typeof navigator !== 'undefined' && /iPad|iPhone|iPod/.test(navigator.userAgent);
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
-  const preppedItems = props.getNavItems.map(item => prepareNavItem(props, item))
-  return (
+  const preppedItems = props.getNavItems.map(item => prepareNavItem(props.pageParams, item));
+
+  const fullMenu = (
     <Box flexDirection="column" className={classes.navMenus}>
       {preppedItems.map(navItem => (
           <NavMenuItem key={navItem.id} {...navItem} />
       ))}
     </Box>
   )
+
+  const mobileMenu = (
+    <SwipeableDrawer
+      anchor="right"
+      disableBackdropTransition={!iOS}
+      disableDiscovery={iOS}
+      open={props.isDrawerOpen}
+      onClose={() => props.setDrawerOpen(false)}
+      onOpen={() => props.setDrawerOpen(true)}
+      classes={{ paper: classes.navMenuMobile}}
+    >
+      <div className={classes.spacer}></div>
+      {preppedItems.map(navItem => (
+        <NavMenuItemMobile key={navItem.id} {...navItem} />
+      ))}
+    </SwipeableDrawer>
+  )
+
+  const displayMenu = isMobile ? mobileMenu : fullMenu;
+
+  console.log('displayMenu', displayMenu);
+
+  return displayMenu;
 }
 
 export default NavMenu
