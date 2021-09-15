5605bb9bcd3e7786cfb37c51c3ba178d12370512
mylons <mrlyons@gmail.com>
2021-09-14 18:23:10 -0700

    deploy to aws script

--- docker/run.sh
+++ docker/run.sh
@@ -7,6 +7,37 @@ if ! which docker 2>&1 > /dev/null; then
     exit 1
 fi
 
-# Create and launch all containers, including graph-node
-docker compose -f docker-compose.yml -f graph-node/docker-compose.yml up --build --force-recreate
+if [ "$1" = "prod" ]; then
+	echo "deploying to prod"
+
+  AWS_ACCOUNT=INSERT AWS ACCOUNT HERE
+  HOST=${AWS_ACCOUNT}.dkr.ecr.us-east-1.amazonaws.com
+
+  aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${HOST}
+
+  docker context use default
+
+  # these are the names of the containers to tag and push
+  # from the docker-compose.yml
+	containers=(reward_coordinator reward_calculator)
+	for container in "${containers[@]}"; do
+	  # create aws ecr repo if not exists
+    aws ecr create-repository --repository-name "${container}" || true
+    cd "${container}"
+    docker build -t "${container}" .
+    docker tag "${container}":latest ${HOST}/"${container}":latest
+    docker push ${HOST}/"${container}":latest
+    cd ..
+	done
+
+  docker context use lithium
+
+  docker compose --file docker-compose-aws.yml up
+
+else
+	# Create and launch all containers, including graph-node
+	echo "deploying locally"
+	docker compose -f docker-compose.yml -f graph-node/docker-compose.yml up --build 
+fi
+
 
