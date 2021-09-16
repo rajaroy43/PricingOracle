871c977f005e32bec3661f88e631319d901f484f
rajaroy43 <rajaroy674@gmail.com>
2021-09-16 16:20:29 +0530

    mock data and unit test added

---
+++ docker/reward_calculator/tests/test_DMI.py
@@ -0,0 +1,10 @@
+import json
+#from reward_calculation import do_calc_np
+from Calculate_rewards import calculate_rewards
+with open('tests/mockData.json') as f:
+  data = json.load(f)
+def test_Should_Calculate_DMI_payments():
+  rewards=calculate_rewards(data)
+  #print(rewards)
+  assert 3==3
+
