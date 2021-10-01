c9750881b894e758a1fe1f8dccc05612b34f6a1a
dtolsen <dtolsen@pixelranch.io>
2021-10-01 18:34:49 -0500

    Disconnect approvelithiumpricing from dashboard.

--- packages/react-app/src/components/forms/ApproveLithiumPricing.tsx
+++ packages/react-app/src/components/forms/ApproveLithiumPricing.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Form } from 'formik'
 import Web3Form from '../formikTLDR/forms/Web3Form'
 import Button from '../atoms/inputs/buttons/Button'
 import Modal from '../atoms/Modal'
 import { parseUnits } from '../../helpers/formatters'
 import { approveSchema } from '../../schemas/approve'
+import { subgraphClient } from '../../client'
+import { WalletContext } from '../providers/WalletProvider'
+import { useGetUser } from '../../queries/user'
+import config from '../../config'
+const MAX_APPROVE = '10000000000'
 
 const Success = () => (
   <div>
@@ -13,28 +18,37 @@ const Success = () => (
 )
 
 const getForm = () => (submit: any, isValid: boolean) => (
-  <Form>
-     
-    <Button
-      label="Submit Approval"
-      onClick={submit}
-      disabled={!isValid}
-    />
-  </Form>
+  <div style={{'display': 'flex', 'justifyContent': 'center', 'padding': '0 0 16px 0'}}>
+    <Form style={{'display': 'flex', 'flexDirection': 'column'}}>
+      <p>Approve $LITH for use in this app.</p>
+      <Button
+        label="Submit Approval"
+        onClick={submit}
+        disabled={!isValid}
+      />
+    </Form>
+  </div>
 )
 
 const getMethodArgs = (pricingAddress: string, value: string) => (_: any) => {
   return [pricingAddress, parseUnits(value)]
 }
 
-const ApproveLithiumPricingForm = ({ connectedAddress, tokenInstance, pricingAddress, value }: any) => {
+const ApproveLithiumPricingForm = () => {
+  const connectedWallet = useContext(WalletContext);
+  // @ts-ignore
+  const { user} = useGetUser(subgraphClient, connectedWallet.address);
+  const pricingIsApproved = user?.pricingIsApproved;
+
   const formProps = {
     defaultValues: approveSchema.defaultValues,
     schema: approveSchema.schema,
     getForm: getForm(),
-    contractMethod: tokenInstance.methods.approve,
-    connectedAddress,
-    getMethodArgs: getMethodArgs(pricingAddress, value),
+    // @ts-ignore
+    contractMethod: connectedWallet.tokenInstance.methods.approve,
+    // @ts-ignore
+    connectedAddress: connectedWallet.address,
+    getMethodArgs: getMethodArgs(config.LITHIUM_PRICING_ADDRESS, MAX_APPROVE),
     stateEls: {
       successEl: Success
     },
@@ -42,18 +56,21 @@ const ApproveLithiumPricingForm = ({ connectedAddress, tokenInstance, pricingAdd
     onSuccess: null
   }
   return (
-    <Modal
-      triggerText='Approve Lithium Pricing'
-      title=''
-      contentText=''
-      getForm={(cancelForm: any) => {
-        return (
-          <Web3Form
-            formProps={{...formProps, cancelForm}}
-          />
-        )
-      }}
-    />
+    <>
+      { !pricingIsApproved && <Modal
+        triggerText='Approve $LITH'
+        title='Approve $LITH'
+        contentText=''
+        getForm={(cancelForm: any) => {
+          return (
+            <Web3Form
+              formProps={{...formProps, cancelForm}}
+            />
+          )
+        }}
+      />
+      }
+    </>
   )
 }
 
--- packages/react-app/src/components/users/UserDashboard.tsx
+++ packages/react-app/src/components/users/UserDashboard.tsx
@@ -6,10 +6,9 @@ import QuestionList from '../questions/QuestionList'
 import UserInputRow from './UserInputRow'
 
 const UserDashboard = ({user, connectedWallet}: {user: UserView, connectedWallet?: any}) => {
-
   return (
     <div>
-      { connectedWallet && <UserInputRow connectedWallet={connectedWallet} pricingIsApproved={user.pricingIsApproved} /> }
+      { connectedWallet && <UserInputRow pricingIsApproved={user.pricingIsApproved} /> }
       <Flex justifyContent='space-around' mt='3em'>
         <QuestionList questions={user.questionViews} />
         <AnswerList answers={user.answerViews} />
