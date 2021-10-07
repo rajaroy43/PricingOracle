95bf6be30ad8dca5a6c5b0d3d5206928aaf5eb36
Steven Ciraolo <steven.ciraolo@gmail.com>
2021-10-07 15:33:39 -0700

    removed off-chain directory

--- packages/off-chain/config/configData.ts
+++
@@ -1,7 +0,0 @@
-const config = {
-  contractAddress: require("./config.json"), //the json containing the smart contract Lithium addresses
-  privateKey: process.env["DEVNET_PRIVKEY"],
-  abiPricing: require("../abis/LithiumPricing.json"),
-  host: "http://127.0.0.1:8545",
-};
-export default config;
