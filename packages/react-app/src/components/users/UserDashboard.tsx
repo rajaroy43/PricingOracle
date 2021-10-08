3fe91ae13b01c19bc427e77b89bdbd6c11e4de82
dtolsen <dtolsen@pixelranch.io>
2021-10-08 17:28:02 -0500

    Editing User Dashboard styling

--- packages/react-app/src/components/users/UserDashboard.tsx
+++ packages/react-app/src/components/users/UserDashboard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
+import Typography from "@material-ui/core/Typography"
 import { UserView } from '../../types/user'
 // import AnswerList from '../answers/AnswerList'
 import Flex from '../atoms/Flex'
@@ -13,18 +14,19 @@ const UserDashboard = ({user, connectedWallet}: {user: UserView, connectedWallet
 
     return (
       <div>
+        <Typography variant="h1">User Dashboard</Typography>
         { connectedWallet && <UserInputRow connectedWallet={connectedWallet} pricingIsApproved={user.pricingIsApproved} /> }
         <Grid container>
           <Grid item md={7} sm={7} xs={12}>
             <UserStats />
           </Grid>
           <Grid item md={5} sm={5} xs={12}>
-            <UserEarnings />
+            <UserEarnings user={user} />
           </Grid>
         </Grid>
         {
         
-        <Flex justifyContent='space-around' mt='3em'>
+        <Flex justifyContent='start'>
           <QuestionList questions={user.questionViews} />
         </Flex>
         }
@@ -32,7 +34,10 @@ const UserDashboard = ({user, connectedWallet}: {user: UserView, connectedWallet
     )
   } else {
     return (
-      <p>Connect wallet to continue</p>
+      <>
+        <Typography variant="h1">Available Questions</Typography>
+        <p>Connect wallet to continue</p>
+      </>
     )
   }
 }
