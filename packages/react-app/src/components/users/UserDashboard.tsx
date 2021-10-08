c175131c03334120a35705064a5f3d5c925c362c
dtolsen <dtolsen@pixelranch.io>
2021-10-08 15:54:56 -0500

    Move stats and earnings to dashboard

--- packages/react-app/src/components/users/UserDashboard.tsx
+++ packages/react-app/src/components/users/UserDashboard.tsx
@@ -1,24 +1,37 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
 import { UserView } from '../../types/user'
 import AnswerList from '../answers/AnswerList'
 import Flex from '../atoms/Flex'
 import QuestionList from '../questions/QuestionList'
+import UserEarnings from '../users/UserEarnings'
 import UserStats from '../users/UserStats'
 import UserInputRow from './UserInputRow'
 
 const UserDashboard = ({user, connectedWallet}: {user: UserView, connectedWallet?: any}) => {
 
+  console.log('user', user);
+  if (connectedWallet) {
+    console.log('connectedWallet', connectedWallet);
+  }
   return (
     <div>
       { connectedWallet && <UserInputRow connectedWallet={connectedWallet} pricingIsApproved={user.pricingIsApproved} /> }
-      <UserStats />
-{/*
+      <Grid container>
+        <Grid item md={7} sm={7} xs={12}>
+          <UserStats />
+        </Grid>
+        <Grid item md={5} sm={5} xs={12}>
+          <UserEarnings />
+        </Grid>
+      </Grid>
+{
       
       <Flex justifyContent='space-around' mt='3em'>
         <QuestionList questions={user.questionViews} />
         <AnswerList answers={user.answerViews} />
       </Flex>
-*/}
+}
     </div>
   )
 }
