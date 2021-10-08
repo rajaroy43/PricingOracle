b52c8f0dddcf859e78ee8146b185c1c839229342
dtolsen <dtolsen@pixelranch.io>
2021-10-08 16:20:40 -0500

    Merge in dev

--- packages/react-app/src/components/users/UserDashboard.tsx
+++ packages/react-app/src/components/users/UserDashboard.tsx
@@ -9,31 +9,32 @@ import UserStats from '../users/UserStats'
 import UserInputRow from './UserInputRow'
 
 const UserDashboard = ({user, connectedWallet}: {user: UserView, connectedWallet?: any}) => {
+  if (user && connectedWallet) {
 
-  console.log('user', user);
-  if (connectedWallet) {
-    console.log('connectedWallet', connectedWallet);
+    return (
+      <div>
+        { connectedWallet && <UserInputRow connectedWallet={connectedWallet} pricingIsApproved={user.pricingIsApproved} /> }
+        <Grid container>
+          <Grid item md={7} sm={7} xs={12}>
+            <UserStats />
+          </Grid>
+          <Grid item md={5} sm={5} xs={12}>
+            <UserEarnings />
+          </Grid>
+        </Grid>
+        {
+        
+        <Flex justifyContent='space-around' mt='3em'>
+          <QuestionList questions={user.questionViews} />
+        </Flex>
+        }
+      </div>
+    )
+  } else {
+    return (
+      <p>Connect wallet to continue</p>
+    )
   }
-  return (
-    <div>
-      { connectedWallet && <UserInputRow connectedWallet={connectedWallet} pricingIsApproved={user.pricingIsApproved} /> }
-      <Grid container>
-        <Grid item md={7} sm={7} xs={12}>
-          <UserStats />
-        </Grid>
-        <Grid item md={5} sm={5} xs={12}>
-          <UserEarnings />
-        </Grid>
-      </Grid>
-{
-      
-      <Flex justifyContent='space-around' mt='3em'>
-        <QuestionList questions={user.questionViews} />
-        {/*<AnswerList answers={user.answerViews} />*/}
-      </Flex>
-}
-    </div>
-  )
 }
 
 export default UserDashboard
