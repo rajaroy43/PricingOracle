44464104251454ef2386b1b046d28359563cf84e
rajaroy43 <rajaroy674@gmail.com>
2021-11-01 21:41:26 +0530

    preventing bad upgrades

---
+++ packages/hardhat/test/upgradeLithium.ts
@@ -0,0 +1,42 @@
+const { ethers ,upgrades} = require("hardhat");
+const { use, expect } = require("chai");
+const { solidity } = require("ethereum-waffle");
+const chaiAsPromised = require("chai-as-promised");
+use(solidity);
+use(chaiAsPromised);
+import { Wallet } from "@ethersproject/wallet";
+import { LithiumPricing } from "../typechain";
+
+describe("Upgrading  Lithium Pricing", async function () {
+  let lithiumPricingProxy: LithiumPricing,
+    account0: Wallet,
+    account1: Wallet;
+
+  beforeEach(async () => {
+    const accounts = await ethers.getSigners();
+    account0 = accounts[0];
+    account1 = accounts[1];
+    const pricingContract = await ethers.getContractFactory("LithiumPricing");
+    lithiumPricingProxy = await upgrades.deployProxy(pricingContract);
+  });
+
+    it("Shound not upgrade if any variable  `maxAnswerSetLength` and `maxAnswerSetLength` is assigned an initial value", async () => {
+    const pricingContractV2 = await ethers.getContractFactory("LithiumPricingInitializingVariable");
+    await expect(upgrades.upgradeProxy(lithiumPricingProxy.address , pricingContractV2)).to.be.rejected 
+    });
+
+    it("Shound not upgrade if having constructor in implementation contract", async () => {
+       const pricingContractV2 = await ethers.getContractFactory("LithiumPricingUsingConstructor");
+       await expect(upgrades.upgradeProxy(lithiumPricingProxy.address , pricingContractV2)).to.be.rejected
+     });
+
+    it("Shound not upgrade if having incompatible storage", async () => {
+       const pricingContractV2 = await ethers.getContractFactory("LithiumPricingIncompatibleStorage");
+       await expect(upgrades.upgradeProxy(lithiumPricingProxy.address , pricingContractV2)).to.be.rejected
+    });
+
+    it("Shound not upgrade if  implementation logic is totally different", async () => {
+         const pricingContractV2 = await ethers.getContractFactory("Roles");
+         await expect(upgrades.upgradeProxy(lithiumPricingProxy.address , pricingContractV2)).to.be.rejected
+    });
+})
