30ca9764f579a80e542fccf6aafbd820a67cb445
rajaroy43 <rajaroy674@gmail.com>
2021-11-22 18:10:19 +0530

    adding multihash script

---
+++ packages/hardhat/scripts/utils/multihash.js
@@ -0,0 +1,30 @@
+const bs58 = require("bs58")
+function getBytes32FromMultiash(multihash) {
+    const decoded = bs58.decode(multihash);
+  
+    return {
+      digest: `0x${decoded.slice(2).toString('hex')}`,
+      hashFunction: decoded[0],
+      size: decoded[1],
+    };
+  }
+
+function getMultihashFromBytes32(multihash) {
+    const { digest, hashFunction, size } = multihash;
+    if (size === 0) return null;
+  
+    // cut off leading "0x"
+    const hashBytes = Buffer.from(digest.slice(2), 'hex');
+    // prepend hashFunction and digest size
+    const multihashBytes = new (hashBytes.constructor)(2 + hashBytes.length);
+    multihashBytes[0] = hashFunction;
+    multihashBytes[1] = size;
+    multihashBytes.set(hashBytes, 2);
+    return bs58.encode(multihashBytes);
+  }
+
+module.exports = {
+    getBytes32FromMultiash,
+    getMultihashFromBytes32
+  };
+  
