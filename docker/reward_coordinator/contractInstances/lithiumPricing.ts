15a20057348f789200600119c2baaaf72c155862
Steven Ciraolo <steven.ciraolo@gmail.com>
2021-12-02 08:58:53 -0800

    added bs58 package

--- docker/reward_coordinator/contractInstances/lithiumPricing.ts
+++ docker/reward_coordinator/contractInstances/lithiumPricing.ts
@@ -32,7 +32,7 @@ export const updateInvalidAnswer = (
 export const addAnswerHashes = (
   updates: AddAnswerHashFields
 ) => { 
-  return lithiumPricing.updateValidAnswerStatus(
+  return lithiumPricing.addAnswerHashes(
     updates.questionIds,
     updates.answerHashes
   ) 
